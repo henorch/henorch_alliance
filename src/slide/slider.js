a256e87ee49cc746bac05c89aa00e3c0de256e55
unknown <henorch01@gmail.com>
2024-08-22 19:34:34 +0100

    logo and background edited

--- src/slide/slider.js
+++ src/slide/slider.js
@@ -1,5 +1,5 @@
 var Slider = document.getElementById('mainSlider');
 
-Slider.style.backgroundImage = "url('../assets/images/images.jpeg')";
+Slider.style.backgroundImage = "url('../../assets/images/images.jpeg')";
 Slider.style.backgroundSize = 'cover';
 Slider.style.backgroundPosition = 'center'
