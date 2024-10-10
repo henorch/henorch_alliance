9ec212df0228d325f0d19576042b9aeb6e5526ff
unknown <henorch01@gmail.com>
2024-10-10 17:57:17 +0100

    product arranged

--- assets/products/products.js
+++ assets/products/products.js
@@ -1,59 +1,73 @@
 const Product = [
     {
         id:1,
-        name:"Catear",
-        image: "../assets/images/game.jpg",
-        price:10000,
-        description:"Ear set for pure sound to your ear"
+        name:"Lontor Blender 1 Extra cup for dry blending",
+        image: "../assets/images/products/Lontor_blender.jpg",
+        price:220000,
+        description:"Lontor 4 Litres blender with dry blender, come with extra cup"
     },
     {
         id:2,
-        name:"Catear",
-        image: "../assets/images/game.jpg",
-        price:10000,
-        description:"Ear set for pure sound to your ear"
+        name:"Plate rack with 3 Level",
+        image: "../assets/images/products/plate_rack.jpg",
+        price:75000,
+        description:"A convinience three level plate rack, for store all kitchen utensils, adequate for family size"
     },
     {
         id:3,
-        name:"Catear",
-        image: "../assets/images/game.jpg",
+        name:"Destiny Double face hot plate",
+        image: "../assets/images/products/2_facehp.jpg",
         price:10000,
-        description:"Ear set for pure sound to your ear"
+        description:"A well ceramic insulated double face eletric stove for fast cooking"
     },
     {
         id:4,
-        name:"Catear",
-        image: "../assets/images/game.jpg",
-        price:10000,
-        description:"Ear set for pure sound to your ear"
+        name:"Kenwood Blender and grinder Uk Standard",
+        image: "../assets/images/products/ke.jpg",
+        price:"45000",
+        description:"Quality 6500 Watts Kenwood industrial blender, for both blending and griding"
     },
     {
         id:5,
-        name:"Sony Dual, Bass free Sound",
-        image: "../assets/images/game.jpg",
-        price:10000,
+        name:"Industrial Lontor Blender",
+        image: "../assets/images/products/blender3.jpg",
+        price:40000,
         description:"Ear set for pure sound to your ear"
     },
     {
         id:6,
-        name:"Catear",
-        image: "../assets/images/game.jpg",
+        name:"Extendable Fan",
+        image: "../assets/images/products/lontor_ext.jpg",
         price:10000,
-        description:"Ear set for pure sound to your ear"
+        description:"A portable, affordable height adjustable table fan, for office and home use"
     },
     {
         id:7,
         name:"Catear",
-        image: "../assets/images/game.jpg",
+        image: "../assets/images/products/catEarDark.jpg",
         price:10000,
         description:"Ear set for pure sound to your ear"
     },
     {
         id:8,
-        name:"Catear",
-        image: "../assets/images/game.jpg",
+        name:"Kenwood Yam Pounder",
+        image: "../assets/images/products/Kem.jpg",
         price:10000,
-        description:"Ear set for pure sound to your ear"
+        description:"Kenwood stainless steel quality yam pounder for family"
+    },
+    {
+        id:9,
+        name:"Bardefu Stainless Blender",
+        image: "../assets/images/products/bard.jpg",
+        price:10000,
+        description:"Quality industrial Bardefu stainless steel blender, for complete blending experiences"
+    },
+    {
+        id:9,
+        name:"Hot Plate 1 FAce",
+        image: "../assets/images/products/1facehp.jpg",
+        price:10000,
+        description:"A well ceramic insulated single face eletric stove for fast cooking"
     },
     ]
 
