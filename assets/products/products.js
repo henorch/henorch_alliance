5b2af17c37f39e57446a840cfde32367700064c4
unknown <henorch01@gmail.com>
2024-10-11 11:11:54 +0100

    everything

--- assets/products/products.js
+++ assets/products/products.js
@@ -3,21 +3,21 @@ const Product = [
         id:1,
         name:"Lontor Blender 1 Extra cup for dry blending",
         image: "../assets/images/products/Lontor_blender.jpg",
-        price:220000,
+        price:45000,
         description:"Lontor 4 Litres blender with dry blender, come with extra cup"
     },
     {
         id:2,
         name:"Plate rack with 3 Level",
         image: "../assets/images/products/plate_rack.jpg",
-        price:75000,
+        price:29000,
         description:"A convinience three level plate rack, for store all kitchen utensils, adequate for family size"
     },
     {
         id:3,
         name:"Destiny Double face hot plate",
         image: "../assets/images/products/2_facehp.jpg",
-        price:10000,
+        price:25000,
         description:"A well ceramic insulated double face eletric stove for fast cooking"
     },
     {
@@ -31,43 +31,57 @@ const Product = [
         id:5,
         name:"Industrial Lontor Blender",
         image: "../assets/images/products/blender3.jpg",
-        price:40000,
+        price:75000,
         description:"Ear set for pure sound to your ear"
     },
     {
         id:6,
         name:"Extendable Fan",
         image: "../assets/images/products/lontor_ext.jpg",
-        price:10000,
+        price:26000,
         description:"A portable, affordable height adjustable table fan, for office and home use"
     },
     {
         id:7,
         name:"Catear",
         image: "../assets/images/products/catEarDark.jpg",
-        price:10000,
+        price:8000,
         description:"Ear set for pure sound to your ear"
     },
     {
         id:8,
         name:"Kenwood Yam Pounder",
         image: "../assets/images/products/Kem.jpg",
-        price:10000,
+        price:38000,
         description:"Kenwood stainless steel quality yam pounder for family"
     },
     {
         id:9,
         name:"Bardefu Stainless Blender",
         image: "../assets/images/products/bard.jpg",
-        price:10000,
+        price:48000,
         description:"Quality industrial Bardefu stainless steel blender, for complete blending experiences"
     },
     {
         id:9,
-        name:"Hot Plate 1 FAce",
+        name:"Hot Plate 1 Face",
         image: "../assets/images/products/1facehp.jpg",
-        price:10000,
+        price:19000,
         description:"A well ceramic insulated single face eletric stove for fast cooking"
+    },
+    {
+        id:9,
+        name:" Kenwood Yam Pounder",
+        image: "../assets/images/products/yanpounder.jpg",
+        price:38000,
+        description:"Kenwood Double speed electric meat grinder, Yam pounder food machine, right for your kitchen"
+    },
+    {
+        id:9,
+        name:"Rechargeable Table fan",
+        image: "../assets/images/products/rechargeablefan.jpg",
+        price:20000,
+        description:"Perfect study/office table top rechargeable fan"
     },
     ]
 
